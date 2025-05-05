55544e287ea679807f1036d3e718bdbd3250bf1c
Pavel_Doroshenko <grelkin07@mail.ru>
2025-05-05 22:44:06 +0700

    методы CRUD для работы с db на сервере.

--- .venv/app.py
+++ .venv/app.py
@@ -1,26 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
-#from populate import DBSession
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Resource_shutdowns, Base
 
 app = Flask(__name__)
 
+#Создание базы данных
 engine = create_engine('sqlite:///shutdowns.db')
 Base.metadata.bind = engine
 
+#Подключение к базе данных
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-#функция отображения всех записей на странице
 @app.route('/')
 def index():
+    """Функция отображения всех записей на странице"""
     resource_shutdowns = session.query(Resource_shutdowns).all()
     return render_template('index.html', resource_shutdowns=resource_shutdowns)
 
-#функия добавления записей
 @app.route('/new', methods=['GET', 'POST'])
 def new_form():
+    """Функция добавления записей"""
     if request.method == 'POST':
         recording_date = request.form['recording_date']
         adress = request.form['adress']
@@ -36,6 +37,7 @@ def new_form():
 
 @app.route('/<int:record_id>/delete/', methods=['GET', 'POST'])
 def delete_record(record_id):
+    """Функция удаления записей"""
     del_record = session.query(Resource_shutdowns).filter_by(id=record_id).one()
     if request.method == 'POST':
         session.delete(del_record)
@@ -45,18 +47,18 @@ def delete_record(record_id):
 
 @app.route('/<int:record_id>/edit/', methods=['GET', 'POST'])
 def edit_record(record_id):
+    """Функция редактирования записей"""
     ed_record = session.query(Resource_shutdowns).filter_by(id=record_id).one()
     if request.method == 'POST':
-        recording_date = request.form['recording_date']
-        adress = request.form['adress']
-        type_of_resource = request.form['type_of_resource']
-        shutdown_date = request.form['shutdown_date']
-        renewal_date = request.form['renewal_date']
-
+        ed_record.recording_date = request.form['recording_date']
+        ed_record.adress = request.form['adress']
+        ed_record.type_of_resource = request.form['type_of_resource']
+        ed_record.shutdown_date = request.form['shutdown_date']
+        ed_record.renewal_date = request.form['renewal_date']
         session.add(ed_record)
         session.commit()
         return redirect(url_for('index', record_id=record_id))
-    return render_template('edit.html')
+    return render_template('edit.html', record = ed_record)
 
 
 if __name__ == '__main__':
